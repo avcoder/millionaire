e570bbade67d4ff00bbbe167b217cdcfdc39fbf1
avcoder <avcoder@users.noreply.github.com>
2025-06-20 14:06:45 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+
+const api =
+  "https://opentdb.com/api.php?amount=15&category=18&type=multiple&encode=url3986";
+
+const money = [
+  { level: "1", amount: "1,000,000" },
+  { level: "2", amount: "500,000" },
+  { level: "3", amount: "250,000" },
+  { level: "4", amount: "100,000" },
+  { level: "5", amount: "50,000" },
+  { level: "6", amount: "25,000" },
+  { level: "7", amount: "16,000" },
+  { level: "8", amount: "8,000" },
+  { level: "9", amount: "4,000" },
+  { level: "10", amount: "2,000" },
+  { level: "11", amount: "1,000" },
+  { level: "12", amount: "500" },
+  { level: "13", amount: "300" },
+  { level: "14", amount: "200" },
+  { level: "15", amount: "100" },
+];
+</script>
+
+<template>
+  <HelloWorld :api="api" :money="money" />
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,203 @@
+<script>
+export default {
+  props: ["api", "money"],
+  data() {
+    return {
+      questions: [],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="app">
+    <main>
+      <div class="header">
+        <!-- Place to place messages or voting results etc -->
+      </div>
+      <h1 class="question" v-html="currentQ"></h1>
+
+      <button id="a" class="item-a">
+        <span>A: </span><span>Possible Answer #1</span>
+      </button>
+      <button id="b" class="item-b">
+        <span>B: </span><span>Possible Answer #2</span>
+      </button>
+      <button id="c" class="item-c">
+        <span>C: </span><span>Possible Answer #3</span>
+      </button>
+      <button id="d" class="item-d">
+        <span>D: </span><span>Possible Answer #4</span>
+      </button>
+    </main>
+
+    <aside>
+      <section>
+        <button id="fifty_fifty">
+          <i class="ms-Icon ms-Icon--Contrast" aria-hidden="true"></i>
+        </button>
+        <button id="ask_audience">
+          <i class="ms-Icon ms-Icon--Group" aria-hidden="true"></i>
+        </button>
+        <button id="phone_friend">
+          <i class="ms-Icon ms-Icon--Phone" aria-hidden="true"></i>
+        </button>
+      </section>
+
+      <ul>
+        <li>15 - $100</li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<style scoped>
+section {
+  width: 400px;
+}
+
+.app {
+  display: flex;
+}
+
+.app ul {
+  list-style-type: none;
+  color: #cad622;
+  font-size: 24px;
+  padding-left: 30%;
+}
+
+.app li {
+  margin-top: 10px;
+}
+.app li:nth-child(1),
+li:nth-child(6),
+li:nth-child(11) {
+  color: #fff;
+}
+.app li span {
+  margin-left: 30px;
+}
+
+main {
+  display: grid;
+  grid-template-columns: 500px 500px 1fr;
+  grid-template-rows: auto;
+  grid-column-gap: 10px;
+  grid-row-gap: 15px;
+  grid-template-areas:
+    "header header header"
+    "question question sidebar"
+    "itema itemb sidebar"
+    "itemc itemd sidebar";
+}
+
+.header {
+}
+
+.question {
+  grid-area: question;
+  justify-self: center;
+  border: 1px solid white;
+  padding: 70px 30%;
+  /* taken from https://uigradients.com/#Lawrencium */
+  background: #0f0c29;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #24243e, #302b63, #0f0c29);
+}
+
+.active {
+  border-top: 1px solid skyblue;
+  border-bottom: 1px solid skyblue;
+}
+
+main button {
+  cursor: pointer;
+  display: block;
+  justify-self: center;
+  font-family: Arial, sans-serif;
+  font-size: 24px;
+  width: 95%;
+  padding: 40px 0;
+  color: #fff;
+  /* taken from https://uigradients.com/#Lawrencium */
+  background: #0f0c29;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #24243e, #302b63, #0f0c29);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #24243e, #302b63, #0f0c29);
+}
+
+main button span {
+  color: #fff;
+}
+
+main button.item-a {
+  grid-area: itema;
+}
+main button.item-b {
+  grid-area: itemb;
+}
+main button.item-c {
+  grid-area: itemc;
+}
+main button.item-d {
+  grid-area: itemd;
+}
+.final_answer {
+  background: #709634 !important;
+}
+.invisible {
+  visibility: hidden !important;
+}
+.ghost {
+  display: none;
+}
+
+aside {
+  grid-area: sidebar;
+  flex-grow: 1;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+aside i {
+  font-size: 80px;
+}
+aside section {
+  text-align: center;
+}
+aside button {
+  cursor: pointer;
+  border-radius: 10px 20px;
+  margin: 10px;
+  color: #fff;
+
+  /* taken from https://uigradients.com/#Lawrencium */
+  background: #000428; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to bottom,
+    #004e92,
+    #000428
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #004e92, #000428);
+}
+
+.settings {
+  margin-top: 100px;
+}
+.settings button {
+  background: grey;
+  margin: 3px;
+  border-radius: 0 0 0 0;
+}
+.settings button i {
+  font-size: 24px;
+}
+
+.header h1 {
+  text-align: center;
+  font-size: 64px;
+}
+</style>
