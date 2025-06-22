b50a3cbe28e38c0d168d07072c2c250056da0765
avcoder <avcoder@users.noreply.github.com>
2025-06-22 07:42:17 -0400

    port over active class to Levels

--- src/components/Levels.vue
+++ src/components/Levels.vue
@@ -5,7 +5,33 @@ export default {
 </script>
 
 <template>
-  <ul>
+  <ul class="levels">
     <li v-for="{ level, amount } in levels">{{ level }} - ${{ amount }}</li>
   </ul>
 </template>
+
+<style>
+.levels {
+  list-style-type: none;
+  color: #cad622;
+  font-size: 24px;
+  padding-left: 30%;
+}
+
+.levels li {
+  margin-top: 10px;
+}
+.levels li:nth-child(1),
+li:nth-child(6),
+li:nth-child(11) {
+  color: #fff;
+}
+.levels li span {
+  margin-left: 30px;
+}
+
+.active {
+  border-top: 1px solid skyblue;
+  border-bottom: 1px solid skyblue;
+}
+</style>
