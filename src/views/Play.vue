7bd763a131c9a1a2b19165312ff1d17cd58fcec2
avcoder <avcoder@users.noreply.github.com>
2025-06-23 18:05:20 -0400

    moved api call to Play

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -8,6 +8,24 @@ import AskHelp from "@/components/AskHelp.vue";
 import PossibleAnswers from "@/components/PossibleAnswers.vue";
 import Levels from "@/components/Levels.vue";
 
+const money = [
+  { level: "1", amount: "100" },
+  { level: "2", amount: "200" },
+  { level: "3", amount: "300" },
+  { level: "4", amount: "500" },
+  { level: "5", amount: "1,000" },
+  { level: "6", amount: "2,000" },
+  { level: "7", amount: "4,000" },
+  { level: "8", amount: "8,000" },
+  { level: "9", amount: "16,000" },
+  { level: "10", amount: "25,000" },
+  { level: "11", amount: "50,000" },
+  { level: "12", amount: "100,000" },
+  { level: "13", amount: "250,000" },
+  { level: "14", amount: "500,000" },
+  { level: "15", amount: "1,000,000" },
+];
+
 export default {
   components: {
     Question,
@@ -15,10 +33,29 @@ export default {
     PossibleAnswers,
     Levels,
   },
-  props: ["api", "questions"],
+  async mounted() {
+    const getData = async () => {
+      const API =
+        "https://opentdb.com/api.php?amount=15&category=18&type=multiple&encode=url3986";
+
+      try {
+        const res = await fetch(API);
+        const data = await res.json();
+        this.questions = data.results
+          .map((q, i) => ({ ...q, ...money[i] }))
+          .reverse();
+        this.nextQ();
+      } catch (e) {
+        console.log("Error fetching data: ", e);
+      }
+    };
+
+    getData();
+  },
   data() {
     return {
       isGameOn: false,
+      questions: [],
       qIndex: -1,
       correctAnswerIndex: 0,
       possibleAnswers: [],
