08236920a3ba1a08585804991d332d6eea3554ed
avcoder <avcoder@users.noreply.github.com>
2025-06-23 18:12:19 -0400

    put money in utils

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,30 +1,12 @@
 <script>
 // utils
-import { getRandomInt, decodeQ } from "@/views/utils.ts";
+import { getRandomInt, decodeQ, money } from "@/views/utils.ts";
 
 // components
 import Question from "@/components/Question.vue";
 import AskHelp from "@/components/AskHelp.vue";
 import PossibleAnswers from "@/components/PossibleAnswers.vue";
 import Levels from "@/components/Levels.vue";
-
-const money = [
-  { level: "1", amount: "100" },
-  { level: "2", amount: "200" },
-  { level: "3", amount: "300" },
-  { level: "4", amount: "500" },
-  { level: "5", amount: "1,000" },
-  { level: "6", amount: "2,000" },
-  { level: "7", amount: "4,000" },
-  { level: "8", amount: "8,000" },
-  { level: "9", amount: "16,000" },
-  { level: "10", amount: "25,000" },
-  { level: "11", amount: "50,000" },
-  { level: "12", amount: "100,000" },
-  { level: "13", amount: "250,000" },
-  { level: "14", amount: "500,000" },
-  { level: "15", amount: "1,000,000" },
-];
 
 export default {
   components: {
--- src/views/utils.ts
+++ src/views/utils.ts
@@ -5,3 +5,21 @@ export function getRandomInt(max = 4) {
 export function decodeQ(encoded: string) {
   return decodeURIComponent(encoded);
 }
+
+export const money = [
+  { level: "1", amount: "100" },
+  { level: "2", amount: "200" },
+  { level: "3", amount: "300" },
+  { level: "4", amount: "500" },
+  { level: "5", amount: "1,000" },
+  { level: "6", amount: "2,000" },
+  { level: "7", amount: "4,000" },
+  { level: "8", amount: "8,000" },
+  { level: "9", amount: "16,000" },
+  { level: "10", amount: "25,000" },
+  { level: "11", amount: "50,000" },
+  { level: "12", amount: "100,000" },
+  { level: "13", amount: "250,000" },
+  { level: "14", amount: "500,000" },
+  { level: "15", amount: "1,000,000" },
+];
